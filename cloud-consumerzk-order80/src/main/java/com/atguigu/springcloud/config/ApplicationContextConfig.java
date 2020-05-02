c3f65ed4be4c9788db766fa8793462ca63c68e0f
yzg <yzgjssz@163.com>
2020-05-02 08:38:38 +0800

    zookeeper 消费者编写cloud-consumerzk-order80

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author：印志刚
+ * @Date：2020/4/26 21:55
+ * @Description：
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
