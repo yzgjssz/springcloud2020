06ff12c279dedd2c055317f31497826b10519700
yzg <yzgjssz@163.com>
2020-07-13 20:14:38 +0800

    Config

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author：印志刚
+ * @Date：2020/7/13 10:40
+ * @Description：
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
